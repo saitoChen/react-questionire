b618a4cef55accaaffd845954bffb5c9f4b0b6e6
chenjianfeng <chenjianfeng9335@gmail.com>
2023-09-16 11:40:06 +0800

    feat: init react-questionire

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/*
+ * @Author: chenjianfeng chenjianfeng9335@gmail.com
+ * @Date: 2023-09-16 11:10:30
+ * @Description: 
+ */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
